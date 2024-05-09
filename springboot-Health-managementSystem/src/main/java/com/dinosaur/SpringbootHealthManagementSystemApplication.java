971a51f990c27605b99bd8f524e6e1d6506458af
Mcz210529 <144803440+Mcz210529@users.noreply.github.com>
2024-05-09 16:38:03 +0800

    Add files via upload

---
+++ springboot-Health-managementSystem/src/main/java/com/dinosaur/SpringbootHealthManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dinosaur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHealthManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHealthManagementSystemApplication.class, args);
+    }
+
+}
